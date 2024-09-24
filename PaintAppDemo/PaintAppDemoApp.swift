e2f180365501952f8e64febd0276d807bc9443b0
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-09-24 20:42:08 +0700

    Initial Commit

---
+++ PaintAppDemo/PaintAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintAppDemoApp.swift
+//  PaintAppDemo
+//
+//  Created by Đoàn Văn Khoan on 24/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PaintAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
